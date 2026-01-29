29b0f9bbd9543aeadfd1ad860af8306b77c9a323
skeksis <neoz.tetrel@gmail.com>
2026-01-29 11:55:13 +0100

    🚧 wip: +[dr_quine] Save sully C

--- Sully/C/Sully.c
+++ Sully/C/Sully.c
@@ -1,8 +1,18 @@
 #include <unistd.h>
 #include <stdio.h>
-#include <stdio.h>
+#include <fcntl.h>
+
+#define CODE "?"
 
 int main(){
 	int count = 5;
-	char name[] = {'S', 'u', 'l', 'l', 'y', '_', count + 48 ,}
+	count--;
+	char name[] = {'S', 'u', 'l', 'l', 'y', '_', count + 48};
+	if (count == 0)
+		return (1);
+	int fd = open(name, O_RDONLY | O_TRUNC, 0644);
+	if (fd == -1)
+		return(1);
+	dprintf(fd, CODE, 10, 34, count, CODE);
+	//system("cc filename && ./a.out");
 }
